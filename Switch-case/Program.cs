dab1a474b67f6bf8f15931de1ba22a4b322b91fc
WanessaT <wanessatlsousa@gmail.com>
2024-06-04 19:01:55 -0300

    Condições switch-case

---
+++ Switch-case/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string valor = "Andre";
+            switch(valor)
+            {
+                case "Joao": Console.WriteLine("Não é o cara!");
+                break;
+                    
+                case "Marcelo": Console.WriteLine("Não é o cara!");
+                break;
+                
+                case "Andre": Console.WriteLine("É o cara!");
+                break;
+
+                default: Console.WriteLine("Não encontrei!");
+                break;
+            }
+
+        }  
+    }
+}
