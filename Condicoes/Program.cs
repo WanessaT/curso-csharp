2122a873e32bd2259aea8c17e42e04ae930c0ffc
WanessaT <wanessatlsousa@gmail.com>
2024-06-04 18:49:24 -0300

    Condições if e else

---
+++ Condicoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Condicoes
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int idade = 18;
+            int maioridade = 21;
+
+            if(idade >= maioridade)
+            {
+                Console.WriteLine("Já é maior de idade.");
+            } 
+            else
+            {
+                Console.WriteLine("Ainda é menor de idade.");
+            }
+                
+
+                Console.WriteLine("Finalizou o programa.");
+        }
+    }
+}
