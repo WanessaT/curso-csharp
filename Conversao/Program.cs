c83f072f04705aef3549628e9d1c565acb66003a
WanessaT <wanessatlsousa@gmail.com>
2024-06-04 17:54:42 -0300

    Primeiro commit

---
+++ Conversao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inteiro = 100;
+            float real = 25.8f;
+
+            //real = inteiro; (conversão implícita)
+            //inteiro = (int)real; (conversão explícita)
+            //inteiro = int.Parse(real.ToString()); (conversão com parse, porém ele só recebe se for string)
+            inteiro = Convert.ToInt32(real); // (conversão usando o Convert)
+
+            Console.WriteLine(inteiro);
+
+        }
+    }
+}
+
---
+++ Operadores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Operadores
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            //Operador aritmético
+            int x = 2;
+            x = (2 + 2) * 5;
+
+            //Operador de atribuição
+            x += 9;
+
+            //Operador de incremento
+            x++;
+
+            Console.WriteLine(x == 30); // == Operador de comparação
+        }
+    }
+}
