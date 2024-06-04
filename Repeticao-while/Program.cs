6cfabf6cdca715cf48c900a5f7f802cae54097a3
WanessaT <wanessatlsousa@gmail.com>
2024-06-04 19:37:52 -0300

    Laços de repetição usando while

---
+++ Repeticao-while/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Repeticao
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+            while(valor <= 5)
+            {
+                Console.WriteLine(valor);
+                valor++;
+            }
+        }
+    }
+}
