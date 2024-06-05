8e549f739ae196fbb0199950a2fcfbe9120b7933
WanessaT <wanessatlsousa@gmail.com>
2024-06-05 11:53:43 -0300

    Montando uma struct

---
+++ Structs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var mouse = new Product(1, "Mouse gamer", 299.90);
+
+          mouse.Id = 55;
+
+          Console.WriteLine(mouse.Id);
+          Console.WriteLine(mouse.Name);
+          Console.WriteLine(mouse.Price);
+        }
+    }
+    
+    struct Product
+    {
+        public Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+        public int Id;
+        public string Name;
+        public double Price;
+
+        public double PriceInDolar(double dolar)
+        {
+            return Price * dolar;
+        }
+    }
+}
