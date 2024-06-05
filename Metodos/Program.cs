7b9bd3e56b6823f9b4927669c89b1606fb42abd3
WanessaT <wanessatlsousa@gmail.com>
2024-06-05 09:28:41 -0300

    Aula prática sobre métodos

---
+++ Metodos/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MeuMetodo();
+
+            string nome = RetornaNome("Wanessa" , "Loureiro");
+            Console.WriteLine(nome);
+        }
+
+        static void MeuMetodo()
+        {
+            Console.WriteLine("C# é legal!");
+        }
+
+        static string RetornaNome(string nome, string sobrenome) //pode ter parâmetros adicionais desde que já tenha atribuição e esteja situado no final. Ex: int idade = 27
+        {
+            return nome + " " + sobrenome;
+        }
+    }
+}
