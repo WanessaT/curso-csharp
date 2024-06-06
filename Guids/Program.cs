1393d5d90a33608e4677476a91d1e420b66de8ad
WanessaT <wanessatlsousa@gmail.com>
2024-06-06 20:43:43 -0300

    Fundamentos do Guid

---
+++ Guids/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            Console.WriteLine(id.ToString().Substring(0 ,8));
+        }
+    }
+}
