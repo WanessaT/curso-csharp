0d30e5ee1657f7cbd028d333933a3cce35ec72ec
WanessaT <wanessatlsousa@gmail.com>
2024-06-06 22:01:52 -0300

    Aula sobre indices

---
+++ Indices/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Indice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.IndexOf("é")); //Acha a posição que o caractere está
+            Console.WriteLine(texto.LastIndexOf("s")); //Acha o indice da ultima vez que a letra ou palavra se repete
+        }
+    }
+}
