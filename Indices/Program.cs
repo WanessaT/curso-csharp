429983a6ac502ebc02e4aafea27b660bc978296f
WanessaT <wanessatlsousa@gmail.com>
2024-06-07 15:48:06 -0300

    Aula sobre manipulação de string

--- Indices/Program.cs
+++ Indices/Program.cs
@@ -6,7 +6,7 @@ namespace Indice
     {
         static void Main(string[] args)
         {
-            var texto = "Este texto é um teste";
+            var texto = " Este texto é um teste ";
             Console.WriteLine(texto.IndexOf("é")); //Acha a posição que o caractere está
             Console.WriteLine(texto.LastIndexOf("s")); //Acha o indice da ultima vez que a letra ou palavra se repete
 
@@ -18,8 +18,20 @@ namespace Indice
             Console.WriteLine(texto.Remove(5, 6)); // Remove informações, nesse caso, a partir da posição 5 ele está removendo os próximos 6 caracteres
             Console.WriteLine(texto.Length); // Conta quando caracteres possui a variavel
 
+            //Manipulação de string
+            Console.WriteLine(texto.Replace("Este", "Isso é um")); //Troca o atual por um novo
 
+            var divisao = texto.Split(" "); // Divide o texto de acordo com o que for definido, nesse caso está quebrando em todos os espaços
+            Console.WriteLine(divisao[0]);
+            Console.WriteLine(divisao[1]);
+            Console.WriteLine(divisao[2]);
+            Console.WriteLine(divisao[3]);
+            Console.WriteLine(divisao[4]);
 
+            var resultado = texto.Substring(5, 5); // Vai até o indice selecionado e captura os proximos de acordo com o indicado
+            Console.WriteLine(resultado);
+
+            Console.WriteLine(texto.Trim()); // Limpa os espaços do começo e fim
         }
     }
 }
--- Indices/obj/Debug/net8.0/Indices.AssemblyInfo.cs
+++ Indices/obj/Debug/net8.0/Indices.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Indices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d30e5ee1657f7cbd028d333933a3cce35ec72ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ff3a6b7d7a546dc5e3fdee40b04ea3db04f08a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Indices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Indices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
