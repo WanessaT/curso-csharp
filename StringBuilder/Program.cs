c0164e9f347cb93e6b62a98abe8adad9ec3e305c
WanessaT <wanessatlsousa@gmail.com>
2024-06-09 10:31:50 -0300

    Aula sobre o método contrutor, StringBuilder

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace StringB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto = new StringBuilder(); // método construtor
+            texto.Append("Este texto é um teste."); // append anexa algo novo
+            texto.Append(" Aqui testamos o append.");
+
+            texto.ToString();
+            Console.WriteLine(texto);
+    
+        }
+    }
+}
