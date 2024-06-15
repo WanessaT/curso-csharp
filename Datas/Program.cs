243cf22a0ff48ef84ea6921762c7479f2a10e86b
WanessaT <wanessatlsousa@gmail.com>
2024-06-15 11:29:43 -0300

    Inicializando com datas

---
+++ Datas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Datas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            //var data = new DateTime(); - Inicializa com a data e hora padrão 01/01
+            var data = DateTime.Now; //mostra a atual
+            Console.WriteLine(data);
+        }
+    }
+}
