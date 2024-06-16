8b33c106d3a4116f2ebd7c83b9790e4e5be22d3f
WanessaT <wanessatlsousa@gmail.com>
2024-06-15 21:02:42 -0300

    Aula: Formatando datas

--- Datas/Program.cs
+++ Datas/Program.cs
@@ -10,9 +10,13 @@ namespace Datas
 
             //var data = new DateTime(); - Inicializa com a data e hora padrão 01/01, porém pode especificar a data colocando entre os ()
             var data = DateTime.Now; //mostra a atual
-            Console.WriteLine(data);
-            Console.WriteLine(data.DayOfWeek);
-            Console.WriteLine(data.DayOfYear);
+            //Console.WriteLine(data);
+            //Console.WriteLine(data.DayOfWeek); // mostra o dia da semana
+            //Console.WriteLine(data.DayOfYear); // mostra o dia do ano
+
+            var formatada = String.Format("{0:dd/MM/yyyy}", data);
+            Console.WriteLine(formatada);
+
         }
     }
 }
--- Datas/obj/Debug/net8.0/Datas.AssemblyInfo.cs
+++ Datas/obj/Debug/net8.0/Datas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Datas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243cf22a0ff48ef84ea6921762c7479f2a10e86b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62a2f07cea94523fdd6f5429e0e59ee37496dc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Datas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Datas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
