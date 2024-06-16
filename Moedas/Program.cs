79edda3e57a39c4b76d10e0d42d9363ac7160f26
WanessaT <wanessatlsousa@gmail.com>
2024-06-16 12:03:50 -0300

    Módulo sobre moedas

---
+++ Moedas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            decimal valor = 200.35m; 
+
+            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine(Math.Round(valor)); //Arredonda o valor 
+            Console.WriteLine(Math.Ceiling(valor)); //Arredonda para cima
+            Console.WriteLine(Math.Floor(valor)); //Arredonda para baixo
+        }
+    }
+}
